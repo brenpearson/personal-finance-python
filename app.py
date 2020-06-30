daab2c7d84b28ddc2f2e0888e2b8f922f340a418
Bren Pearson <bren.pearson0100@gmail.com>
2020-06-30 17:28:20 +1000

    Try this time Heroku

--- app.py
+++ app.py
@@ -12,4 +12,6 @@ def home():
 def getAccount(id):
     return jsonify(Accounts.getAccount(id))
 
-app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
