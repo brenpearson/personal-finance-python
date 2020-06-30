454e0de5b771c6590895cd8c3b25e17fa77c65a3
Bren Pearson <bren.pearson0100@gmail.com>
2020-06-30 17:22:00 +1000

    Add home

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import accounts as Accounts
 
 app = flask.Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def home():
+    return 'Hello!'
+
 @app.route('/accounts/<id>', methods=['GET'])
 def getAccount(id):
     return jsonify(Accounts.getAccount(id))
